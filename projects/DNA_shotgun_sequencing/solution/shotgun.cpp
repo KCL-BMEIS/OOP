acaf51e552c9ddf2a10955acfe3804efd105df10
J-Donald Tournier <jdtournier@gmail.com>
2024-11-14 19:20:23 +0000

    Separate error handling from core functionality
    To de-clutter the code further, we can introduce a function containing
    the code that we want to run, and invoke that function from `main()`
    with all the error handling done there. This keeps the important code
    free of error handling, and easier to read and maintain.
    
    We also take the opportunity to pass the arguments to our new function
    in the expected format. This leaves a skeleton main() function that we
    can leave as-is, and we can now focus on the core functionality.

--- projects/DNA_shotgun_sequencing/solution/shotgun.cpp
+++ projects/DNA_shotgun_sequencing/solution/shotgun.cpp
@@ -4,32 +4,42 @@
 
 #include "fragments.h"
 
+// This function contains our program's core functionality:
+
+void run (std::vector<std::string>& args)
+{
+  if (args.size() < 3)
+    throw std::runtime_error ("missing arguments - expected 2 arguments: input_fragments output_sequence");
+
+  auto fragments = load_fragments (args[1]);
+  fragment_statistics (fragments);
+
+  unsigned int size_of_longest = 0;
+  unsigned int index_of_longest = 0;
+  for (unsigned int n = 0; n < fragments.size(); ++n) {
+    if (fragments[n].size() > size_of_longest) {
+      index_of_longest = n;
+      size_of_longest = fragments[n].size();
+    }
+  }
+  std::string sequence = fragments[index_of_longest];
+  std::cerr << "initial sequence has size " << sequence.size() << "\n";
+
+  write_sequence (args[2], sequence);
+}
+
+
+
+
+// skeleton main() function, whose purpose is now to pass the arguments to
+// run() in the expected format, and catch and handle any exceptions that may
+// be thrown.
+
 int main (int argc, char* argv[])
 {
-  std::vector<std::string> args (argv, argv+argc);
-
   try {
-
-    if (args.size() < 3)
-      throw std::runtime_error ("missing arguments - expected 2 arguments: input_fragments output_sequence");
-
-    auto fragments = load_fragments (args[1]);
-    fragment_statistics (fragments);
-
-    unsigned int size_of_longest = 0;
-    unsigned int index_of_longest = 0;
-    for (unsigned int n = 0; n < fragments.size(); ++n) {
-      if (fragments[n].size() > size_of_longest) {
-        index_of_longest = n;
-        size_of_longest = fragments[n].size();
-      }
-    }
-
-    std::string sequence = fragments[index_of_longest];
-    std::cerr << "initial sequence has size " << sequence.size() << "\n";
-
-    write_sequence (args[2], sequence);
-
+    std::vector<std::string> args (argv, argv+argc);
+    run (args);
   }
   catch (std::exception& excp) {
     std::cerr << "ERROR: " << excp.what() << " - aborting\n";
