5cec181668bc27c4d618ad448d16375cd88e02ee
J-Donald Tournier <jdtournier@gmail.com>
2024-11-15 17:44:05 +0000

    Keep iterating while fragments remain to be merged
    Now that the pieces are all in place, we can complete the algorithm, and
    keep iterating until no further fragments remain.

--- projects/DNA_shotgun_sequencing/solution/overlap.cpp
+++ projects/DNA_shotgun_sequencing/solution/overlap.cpp
@@ -10,10 +10,6 @@ int compute_overlap (const std::string& sequence, const std::string& fragment)
   if (fragment.size() > sequence.size())
     throw std::runtime_error ("fragment size larger than current sequence - this shouldn't happen!");
 
-  std::cerr << "------------------------------------------------------\n";
-  std::cerr << "sequence is \"" << sequence << "\"\n";
-  std::cerr << "trying fragment \"" << fragment << "\"\n";
-
   int largest_overlap = 0;
 
   // Start from the largest overlap, and decrease size of overlap at each
@@ -21,7 +17,6 @@ int compute_overlap (const std::string& sequence, const std::string& fragment)
   for (int overlap = fragment.size(); overlap > 0; --overlap) {
     const auto seq_start = sequence.substr(0, overlap);
     const auto frag_end = fragment.substr(fragment.size()-overlap);
-    //std::cerr << "  overlap = " << overlap << ": seq " << seq_start << ", fragment " << frag_end << "\n";
     if (seq_start == frag_end) {
       largest_overlap = overlap;
       break;
@@ -34,7 +29,6 @@ int compute_overlap (const std::string& sequence, const std::string& fragment)
   for (int overlap = fragment.size(); overlap > largest_overlap; --overlap) {
     const auto seq_end = sequence.substr(sequence.size() - overlap);
     const auto frag_start = fragment.substr(0, overlap);
-    //std::cerr << "  overlap = " << overlap << ": seq " << seq_end << ", fragment " << frag_start << "\n";
     if (seq_end == frag_start) {
       largest_overlap = -overlap;
       break;
