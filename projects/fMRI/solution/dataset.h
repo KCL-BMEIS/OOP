59984923a853bed6b538462aaf848e1dcf3f6969
J-Donald Tournier <jdtournier@gmail.com>
2024-12-03 12:33:32 +0000

    Convert the Dataset into a template class as well
    We can also use template programming for the Dataset class, making it
    capable of holding a set of images of arbitrary types for the pixel
    values.
    
    In this case however, we need to make sure any template function
    or method definitions are also included in the header. If left in the
    cpp file, the compiler will not be able to generate the code for the
    concrete realisation of the class where it is needed. For example, if we
    use the Dataset<int> class in our main run() function, the compiler will
    need to have access to the code for all the functions and methods we
    rely on when it compiles the fmri.cpp file (where the run() function is
    defined).
    
    Conversely, if we left the definition of these functions in
    the dataset.cpp file, the compiler wouldn't know which versions we might
    be using in any other parts of our program when compiling dataset.cpp.
    Should it generate code for Dataset<int>, Dataset<float>,
    Dataset<double>, etc.? In general, there are too many possible types
    here, so the compiler will only check for correct syntax: it will
    generate no code at all. This will lead to link-time errors later on,
    which look like 'undefined symbol' errors.

--- projects/fMRI/solution/dataset.h
+++ projects/fMRI/solution/dataset.h
@@ -2,9 +2,13 @@
 
 #include <vector>
 #include <string>
+#include <format>
 
 #include "image.h"
+#include "pgm.h"
+#include "debug.h"
 
+template <typename ValueType>
 class Dataset
 {
   public:
@@ -15,20 +19,62 @@ class Dataset
 
     unsigned int size () const { return m_slices.size(); }
 
-    Image<int>&       operator[] (int n)       { return m_slices[n]; }
-    const Image<int>& operator[] (int n) const { return m_slices[n]; }
+    Image<ValueType>&       operator[] (int n)       { return m_slices[n]; }
+    const Image<ValueType>& operator[] (int n) const { return m_slices[n]; }
 
-    std::vector<int> get_timecourse (int x, int y) const;
+    std::vector<ValueType> get_timecourse (int x, int y) const;
 
   private:
-    std::vector<Image<int>> m_slices;
+    std::vector<Image<ValueType>> m_slices;
 };
 
 
-inline std::ostream& operator<< (std::ostream& out, const Dataset& data)
+template <typename ValueType>
+inline std::ostream& operator<< (std::ostream& out, const Dataset<ValueType>& data)
 {
   out << "Data set with " << data.size() << " images:\n";
   for (unsigned int n = 0; n < data.size(); ++n)
     out << "  image " << n << ": " << data[n] << "\n";
   return out;
 }
+
+
+
+
+template <typename ValueType>
+void Dataset<ValueType>::load (const std::vector<std::string>& filenames)
+{
+  m_slices.clear();
+
+  if (filenames.empty())
+    throw std::runtime_error ("no filenames supplied when loading dataset");
+
+  for (const auto& fname : filenames)
+    m_slices.push_back (load_pgm (fname));
+
+  // check that dimensions all match up:
+  for (unsigned int n = 1; n < m_slices.size(); ++n) {
+    if ( (m_slices[n].width() != m_slices[n-1].width()) ||
+         (m_slices[n].height() != m_slices[n-1].height()) )
+      throw std::runtime_error ("dimensions do not match across slices");
+  }
+
+  debug::log (std::format (
+      "loaded {} slices of size {}x{}\n",
+      m_slices.size(), m_slices[0].width(), m_slices[0].height()));
+}
+
+
+
+
+
+template <typename ValueType>
+std::vector<ValueType> Dataset<ValueType>::get_timecourse (int x, int y) const
+{
+  std::vector<ValueType> vals (size());
+  for (unsigned int n = 0; n < size(); ++n)
+    vals[n] = m_slices[n](x,y);
+  return vals;
+}
+
+
