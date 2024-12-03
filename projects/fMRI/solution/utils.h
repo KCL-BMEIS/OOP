fee784183be3e5641b1a1a6a3ba9c4f158d1cf2d
J-Donald Tournier <jdtournier@gmail.com>
2024-12-03 16:34:06 +0000

    Add function to compute image of correlation coefficients
    We now have all the pieces required to complete the project, with a
    function to loop over all the pixels in the input data, and compute the
    correlation coefficient of each pixel's timecourse with the task.
    
    Since our Image class can only store data of type int, we cannot store
    the correlation coefficient values as they are, since they scale from -1
    to 1. We therefore scale the results by 1000 to store them in the
    resulting image of coefficients.

--- projects/fMRI/solution/utils.h
+++ projects/fMRI/solution/utils.h
@@ -2,5 +2,10 @@
 
 #include <vector>
 
+#include "image.h"
+#include "dataset.h"
+
 double correlation_coefficient (const std::vector<int>& a, const std::vector<int>& b);
+
+Image correlation_coefficient (const std::vector<int>& task, const Dataset& data);
 
