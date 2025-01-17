43f4cfd6b41b04b1ba9d70eefc827ce50e387316
J-Donald Tournier <jdtournier@gmail.com>
2025-01-17 17:17:23 +0000

    Introduce template function for rescale() and correlation_coefficient()
    C++ allows us to define template functions, that will operate on generic
    types. The actual type to be used in the function is not know at the
    point where it declared & defined. At the point where the function is
    used, the compiler will substitute the desired type, and produce the
    machine code for that specific version of the function at that point.
    
    This allows us to define generic functions that can be used with any
    (compatible) type, and avoid unnecessary code duplication.

--- projects/fMRI/solution/task.h
+++ projects/fMRI/solution/task.h
@@ -5,5 +5,14 @@
 
 std::vector<int> load_task (const std::string& filename);
 
-std::vector<float> rescale (const std::vector<int>& task, int min, int max);
+template <typename ValueType>
+std::vector<float> rescale (const std::vector<ValueType>& task, ValueType min, ValueType max)
+{
+  std::vector<float> out (task.size());
+  for (unsigned int n = 0; n < task.size(); ++n)
+    out[n] = min + task[n] * (max-min);
+  return out;
+}
+
+
 
--- projects/fMRI/solution/utils.h
+++ projects/fMRI/solution/utils.h
@@ -1,11 +1,36 @@
 #pragma once
 
 #include <vector>
+#include <numeric>
 
 #include "image.h"
 #include "dataset.h"
 
-double correlation_coefficient (const std::vector<int>& a, const std::vector<int>& b);
+
+template <typename VectorType1, typename VectorType2>
+double correlation_coefficient (const VectorType1& a, const VectorType2& b)
+{
+  if (a.size() != b.size())
+    throw std::runtime_error ("size mismatch for vectors in correlation_coefficient()");
+
+  const double mean_a = std::accumulate (a.begin(), a.end(), 0.0) / a.size();
+  const double mean_b = std::accumulate (b.begin(), b.end(), 0.0) / b.size();
+
+  double numerator = 0.0;
+  double sos_a = 0.0, sos_b = 0.0;
+
+  for (unsigned int n = 0; n < a.size(); ++n) {
+    const double diff_a = a[n] - mean_a;
+    const double diff_b = b[n] - mean_b;
+    numerator += diff_a * diff_b;
+    sos_a += diff_a * diff_a;
+    sos_b += diff_b * diff_b;
+  }
+
+  return numerator / ( std::sqrt(sos_a) * std::sqrt(sos_b) );
+}
+
 
 Image correlation_coefficient (const std::vector<int>& task, const Dataset& data);
+
 
